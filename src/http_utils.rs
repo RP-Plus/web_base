0b4b1742f907d9b0d95521d94e56c7918b6ecd81
Emma Davenport <davenport.physics@gmail.com>
2023-02-17 12:48:47 -0600

    Addition: try_path_var in HttpUtils

--- src/http_utils.rs
+++ src/http_utils.rs
@@ -5,6 +5,7 @@ use actix_web::{HttpRequest};
 pub trait HttpUtils {
 
 	fn path_var<T>(&self, path_var: &str) -> T where T: FromStr, <T as FromStr>::Err: std::fmt::Debug;
+	fn try_path_var<T>(&self, path_var: &str) -> Result<T, String> where T: FromStr, <T as FromStr>::Err: std::fmt::Debug;
 
 }
 
@@ -13,6 +14,15 @@ impl HttpUtils for HttpRequest {
 	fn path_var<T>(&self, path_var: &str) -> T where T: FromStr, <T as FromStr>::Err: std::fmt::Debug {
 
 		self.match_info().get(path_var).unwrap().parse().unwrap()
+
+	}
+
+	fn try_path_var<T>(&self, path_var: &str) -> Result<T, String> where T: FromStr, <T as FromStr>::Err: std::fmt::Debug {
+
+		match self.match_info().get(path_var).unwrap().parse() {
+			Ok(var) => return Ok(var),
+			Err(_err) => Err(format!("Unable to parse {}", path_var))
+		}
 
 	}
 
