19511b695a120f464ee4a7ea9b119402a34612a0
Emma Davenport <davenport.physics@gmail.com>
2023-03-16 19:58:25 -0500

    Addition: ip_addr function to HttpUtils trait

--- src/http_utils.rs
+++ src/http_utils.rs
@@ -1,9 +1,11 @@
 
+use std::net::SocketAddr;
 use std::str::FromStr;
 use actix_web::{HttpRequest};
 
 pub trait HttpUtils {
 
+	fn ip_addr(&self) -> Result<String, String>;
 	fn path_var<T>(&self, path_var: &str) -> T where T: FromStr, <T as FromStr>::Err: std::fmt::Debug;
 	fn try_path_var<T>(&self, path_var: &str) -> Result<T, String> where T: FromStr, <T as FromStr>::Err: std::fmt::Debug;
 
@@ -23,6 +25,26 @@ impl HttpUtils for HttpRequest {
 			Ok(var) => return Ok(var),
 			Err(_err) => Err(format!("Unable to parse {}", path_var))
 		}
+
+	}
+
+	fn ip_addr(&self) -> Result<String, String> {
+
+		let info = self.connection_info();
+		if let Some(ip_temp) = info.realip_remote_addr() {
+
+			let mut temp: String = String::from(ip_temp);
+			temp.push_str(":9999");
+
+			return match temp.parse::<SocketAddr>() {
+
+				Ok(address) => Ok(address.ip().to_string()),
+				Err(_msg) => Err("Invalid ip".to_string()) 
+
+			};
+
+		}
+		Err("Unable to determin ip address".to_string()) 
 
 	}
 
